15972ad794aba1c44120a629509326962b174006
Parker Morgan <jpmaster13@msn.com>
2021-03-01 14:20:04 -0700

    components updated to sort the array

---
+++ src/components/Card.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Button from 'react-bootstrap/Button';
+
+function Card(props) {
+console.log(props)
+  return (
+    <div className="card text-dark p-2">
+      <div className="card-header bg-info text-light">Love seekers near you!</div>
+      <div className="card-body row">
+        <div className="col">
+          <h3 className="card-text ">
+            {props.person.name.title} {props.person.name.first} {props.person.name.last} 
+          </h3>
+          <p className="card-text">
+            Gender: {props.person.gender}
+          </p>
+          <p className="card-text">
+            Cellphone Number: {props.person.cell}
+          </p>
+          <p className="card-text">
+            Country: {props.person.location.country}
+          </p> 
+
+        </div>
+
+        <img src={props.person.picture.large} className="col img-fluid" />
+      </div>
+      <div>
+        <p className="card-text row p-2">login: {props.person.login.username}</p>
+        <p className="card-text row p-2">password: {props.person.login.password}</p>
+       </div>
+       <div>
+       <Button variant="info" size="small">Meet me</Button>
+       </div>
+      
+    </div>
+  );
+}
+
+export default Card;
---
+++ src/components/Jumbotron.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Jumbotron() {
+  return (
+    <div className="jumbotron text-dark">
+      <h1>Love finder</h1>
+      <p>
+        Are you alonely looking for love within your area?! With love finders we have found a way to match local lonely people to you and your personality type!
+      </p>
+    </div>
+  );
+}
+
+export default Jumbotron;
